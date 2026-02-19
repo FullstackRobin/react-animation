9ef9c743c608d0726b3126cbb2c17c494ba7b4d4
FullstackRobin <Fullstackrobin@gmail.com>
2026-02-19 01:37:08 +0100

    Reapply "uploading files"
    This reverts commit 09155a44761ba59eb7f898cbf00e731b07898d5f.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {motion} from "motion/react";
+import AnimatedButton from './components/AnimatedButton'
+
+function App() {
+
+  return (
+    <>
+    <AnimatedButton>Get Started</AnimatedButton>
+    </>
+  );
+} 
+
+export default App;
